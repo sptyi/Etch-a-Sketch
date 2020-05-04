3c45563c7470f3af8ab0bf76483b95b85ac7f45e
sptyi <cjhilaman@yahoo.com>
2020-05-03 22:21:44 -0400

    Add functionality. Change style.

--- eas.js
+++ eas.js
@@ -1,11 +1,42 @@
 const container = document.querySelector('#container');
 const btnDiv = document.createElement('div');
-var resetBtn = document.createElement('button');
 container.appendChild(btnDiv);
-window.onload = function () {
-	createDivGrid();
-	createResetBtn();
-};
+var resetBtn = document.createElement('button');
+var gridSize;
+
+createResetBtn();
+setGridSize();
+createDivGrid(gridSize);
+
+function setGridSize() {
+	gridSize = Math.pow(
+		prompt(
+			'What size grid would you like to use for your canvas? (For instance, entering 16 would give you a 16x16 square canvas.)',
+			16
+		),
+		2
+	);
+	if (gridSize == '') {
+		gridSize = 256;
+	} else if (gridSize) {
+		return;
+	} else {
+		gridSize = 256;
+	}
+}
+
+function createDivGrid(gridSize) {
+	for (i = 0; i < gridSize; i++) {
+		const div = document.createElement('div' + i);
+		container.appendChild(div);
+		div.classList.add('square');
+		div.style.cssText =
+			'display: inline-block; min-width: 10px; min-height: 10px; max-width: 10px; max-height: 10px; border: 1px solid whitesmoke; margin-right: 5px;';
+		div.addEventListener('mouseover', () => {
+			div.style.cssText += 'background-color: red;';
+		});
+	}
+}
 
 function createResetBtn() {
 	btnDiv.appendChild(resetBtn);
@@ -20,23 +51,6 @@ resetBtn.addEventListener('click', () => {
 	for (i = 0; i < squares.length; i++) {
 		squares[i].style.cssText += 'background-color: transparent;';
 	}
-	var gridSize = prompt(
-		'What size grid would you like to use for your canvas?'
-	);
-	if ((gridSize = null)) {
-		gridSize = 256;
-	}
+	setGridSize();
+	createDivGrid(gridSize);
 });
-
-function createDivGrid() {
-	for (i = 0; i < 256; i++) {
-		const div = document.createElement('div' + i);
-		container.appendChild(div);
-		div.classList.add('square');
-		div.style.cssText =
-			'display: inline-block; width: 10px; height: 10px; border: 1px solid whitesmoke; margin-right: 5px;';
-		div.addEventListener('mouseover', () => {
-			div.style.cssText += 'background-color: red;';
-		});
-	}
-}
