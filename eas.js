ca0ce81c145e4f63800501414131c410e9ce97c1
sptyi <cjhilaman@yahoo.com>
2020-05-08 15:26:23 -0400

    Add loading visual during computational loop. Revise style.

--- eas.js
+++ eas.js
@@ -1,9 +1,10 @@
-document.getElementsByTagName("body")[0].setAttribute("id", "body");
-const body = document.querySelector("#body");
-const btnDiv = document.createElement("div");
+document.getElementsByTagName('body')[0].setAttribute('id', 'body');
+const body = document.querySelector('#body');
+const btnDiv = document.createElement('div');
 body.insertBefore(btnDiv, body.childNodes[1]);
-const resetBtn = document.createElement("button");
-const container = document.querySelector("#container");
+const resetBtn = document.createElement('button');
+const container = document.querySelector('#container');
+const loading = document.querySelector('.loading');
 var gridSize;
 var squareSize;
 var squaresPerSide;
@@ -12,14 +13,16 @@ createResetBtn();
 
 function createResetBtn() {
 	btnDiv.appendChild(resetBtn);
-	resetBtn.innerHTML = "Reset Canvas";
+	resetBtn.innerHTML = 'Reset Canvas';
 	resetBtn.style.cssText =
 		'outline: none; margin-bottom: 50px; margin-top: 25px; padding: 5px; border-radius: 7px';
-	setGridSize();
+	squaresPerSide = 16;
+	gridSize = 256;
 	createDivGrid(gridSize);
 	resetBtn.addEventListener('click', () => {
 		setGridSize();
 		createDivGrid(gridSize);
+		hideLoading();
 	});
 }
 
@@ -28,18 +31,25 @@ function setGridSize() {
 		'What size grid would you like to use for your canvas? \n (For instance, entering 16 would give you a 16x16 square canvas.)'
 	);
 	console.log(squaresPerSide);
-	if (squaresPerSide == '') {
+	if (squaresPerSide > 500) {
+		alert(
+			'The canvas grid size cannot exceed 500. \n Please enter a number less than 501.'
+		);
+		setGridSize();
+	} else if (squaresPerSide == '') {
 		squaresPerSide = 16;
 		gridSize = Math.pow(squaresPerSide, 2);
 		console.log(gridSize);
+		createLoading();
 	} else if (squaresPerSide) {
 		gridSize = Math.pow(squaresPerSide, 2);
 		console.log(gridSize);
-		return;
+		createLoading();
 	} else {
 		squaresPerSide = 16;
 		gridSize = Math.pow(squaresPerSide, 2);
 		console.log(gridSize);
+		createLoading();
 	}
 }
 
@@ -47,15 +57,29 @@ function createDivGrid(gridSize) {
 	while (container.firstChild) {
 		container.removeChild(container.lastChild);
 	}
-	for (i = 0; i < gridSize; i++) {
-		const div = document.createElement('div');
-		container.appendChild(div);
-		div.classList.add('square');
-		div.addEventListener('mouseover', () => {
-			div.style.cssText += 'background-color: black;';
-		});
-		setGridProperties(squareSize);
-	}
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			for (i = 0; i < gridSize; i++) {
+				const div = document.createElement('div');
+				container.appendChild(div);
+				div.classList.add('square');
+				div.addEventListener('mouseover', () => {
+					div.style.cssText += 'background-color: black;';
+				});
+				setGridProperties(squareSize);
+			}
+			resolve('loaded');
+		}, 1000);
+	});
+}
+
+async function hideLoading() {
+	const loaded = await createDivGrid();
+	loading.style.display = 'none';
+}
+
+function createLoading() {
+	loading.style.display = 'block';
 }
 
 function setGridProperties(squareSize) {
